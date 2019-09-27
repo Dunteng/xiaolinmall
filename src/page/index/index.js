b6920b3b14dc17adae51d30b08be46ed747633fd
Dunteng <lindunteng@gmail.com>
2019-09-27 17:05:16 +0800

    项目初始化和通用功能开发1-4

---
+++ src/page/index/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+require('page/common/nav-simple/index.js')
+require('page/common/nav/index.js')
+require('page/common/header/index.js')
+var navSide = require('page/common/nav-side/index.js');
+var _mm = require('util/mm.js');
+
+navSide.init({
+    name: "user-center"
+})
+
